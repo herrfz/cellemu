9ca52984d88cfbf5e8ef7fc7ee203db193f6ee98
herrfz <erizzaaaaa@gmail.com>
2014-08-26 15:48:42 +0200

    implemented serial error handling

--- worker/serial.go
+++ worker/serial.go
@@ -2,15 +2,19 @@
 package worker
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/tarm/goserial"
 	"io"
+	"os"
 )
 
 func DoSerial(dl_chan, ul_chan chan []byte) {
 	c := &serial.Config{Name: "/dev/tty0", Baud: 57600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
-		panic(err) // TODO
+		fmt.Println("error opening serial interface:", err.Error())
+		os.Exit(1)
 	}
 
 	go read_from_serial(s, ul_chan)
@@ -22,9 +26,11 @@ func read_from_serial(s io.ReadWriteCloser, ul_chan chan []byte) {
 		buf := make([]byte, 128)
 		n, err := s.Read(buf)
 		if err != nil {
-			panic(err) // TODO
+			fmt.Println("error reading from serial:", err.Error())
+			continue
 		}
 		ul_chan <- buf[:n]
+		fmt.Println("read from serial:", hex.EncodeToString(buf[:n]))
 	}
 }
 
@@ -33,7 +39,9 @@ func write_to_serial(dl_chan chan []byte, s io.ReadWriteCloser) {
 		buf := <-dl_chan
 		_, err := s.Write(buf)
 		if err != nil {
-			panic(err) // TODO
+			fmt.Println("error writing to serial:", err.Error())
+			continue
 		}
+		fmt.Println("written to serial:", hex.EncodeToString(buf))
 	}
 }
