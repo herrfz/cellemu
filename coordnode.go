6ea9ac1f34eb83996f82895f6f23eec7d85ceab1
herrfz <erizzaaaaa@gmail.com>
2014-09-25 18:37:20 +0200

    new feature: connect to wdc via serial

--- coordnode.go
+++ coordnode.go
@@ -5,32 +5,31 @@ import (
 	"fmt"
 	work "github.com/herrfz/coordnode/worker"
 	"github.com/herrfz/gowdc/utils"
-	zmq "github.com/pebbe/zmq4"
+	"github.com/tarm/goserial"
+	"io"
 	"os"
 	"os/signal"
 )
 
-type Socket struct {
-	socket *zmq.Socket
+type SerialReader struct {
+	serial io.ReadWriteCloser
 }
 
-func (sock Socket) ReadDevice() ([]byte, error) {
-	buf, err := sock.socket.Recv(0)
-	if err != nil {
-		if err.Error() == "Operation cannot be accomplished in current state" {
-			// give some time for REP socket to send before another recv
-			return nil, fmt.Errorf("DONTPANIC")
-		}
+func (s SerialReader) ReadDevice() ([]byte, error) {
+	buf := make([]byte, 128)
+	msglen, _ := s.serial.Read(buf)
+	if msglen > 0 {
+		return buf[:msglen], nil
+	} else {
+		return []byte{}, nil
 	}
-	return []byte(buf), err
 }
 
 func main() {
-	serial := flag.Bool("serial", false, "use serial port to talk to sensor node")
 	device := flag.String("device", "", "serial device to use")
 	flag.Parse()
 
-	if *serial && *device == "" {
+	if *device == "" {
 		fmt.Println("no serial device provided")
 		os.Exit(1)
 	}
@@ -38,54 +37,37 @@ func main() {
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt)
 
-	c_sock, _ := zmq.NewSocket(zmq.REP)
-	defer c_sock.Close()
-	c_sock.Bind("tcp://*:5555")
+	siface := &serial.Config{Name: *device, Baud: 9600}
+	s, err := serial.OpenPort(siface)
+	if err != nil {
+		fmt.Println("error opening serial interface:", err.Error())
+		os.Exit(1)
+	}
+	defer s.Close()
 
-	d_dl_sock, _ := zmq.NewSocket(zmq.PULL)
-	defer d_dl_sock.Close()
-	d_dl_sock.Connect("tcp://localhost:5556")
-
-	d_ul_sock, _ := zmq.NewSocket(zmq.PUSH)
-	defer d_ul_sock.Close()
-	d_ul_sock.Bind("tcp://*:5557")
+	serial := SerialReader{s}
+	wdcch := utils.MakeChannel(serial)
 
 	dl_chan := make(chan []byte)
 	ul_chan := make(chan []byte)
-
-	if *serial {
-		go work.DoSerialDataRequest(dl_chan, ul_chan, *device)
-	} else {
-		go work.DoDataRequest(dl_chan, ul_chan)
-	}
-
-	data_ch := utils.MakeChannel(Socket{d_dl_sock})
-	cmd_ch := utils.MakeChannel(Socket{c_sock})
+	go work.DoDataRequest(dl_chan, ul_chan)
 
 LOOP:
 	for {
 		select {
-		case buf := <-cmd_ch:
-			respmsg := work.ProcessMessage([]byte(buf))
+		case buf := <-wdcch:
+			respmsg := work.ProcessMessage(buf)
 			if respmsg != nil {
-				c_sock.Send(string(respmsg), 0)
-				fmt.Println("sent answer to TCP command")
-			}
-
-		case buf := <-data_ch:
-			respmsg := work.ProcessMessage([]byte(buf))
-			if respmsg != nil {
-				d_ul_sock.Send(string(respmsg), 0)
-				fmt.Println("sent answer to UDP mcast message")
+				s.Write(respmsg)
+				fmt.Println("sent answer to WDC request")
 			}
 			// if buf is MAC_DATA_REQUEST, pass it to handler goroutine
-			// can either be local handler or serial forwarder
 			if len(buf) != 0 && buf[1] == 0x17 {
-				dl_chan <- []byte(buf)
+				dl_chan <- buf
 			}
 
 		case buf := <-ul_chan:
-			d_ul_sock.Send(string(buf), 0)
+			s.Write(buf)
 			fmt.Println("sent node uplink message")
 
 		case <-c:
