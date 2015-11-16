71ad3a4527eacf74636687fe856f32c406bb19de
Eriza Fazli <erizzaaaaa@gmail.com>
2015-11-16 23:02:26 +0100

    use context manager for serial in test script

--- app/nfctest.py
+++ app/nfctest.py
@@ -1,12 +1,8 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/pts/32', 57600)
-
-with open('trace.txt', 'r') as f:
+with open('trace.txt', 'r') as f, serial.Serial('/dev/pts/31', 57600) as ser:
     for line in f:
         print(line)
         ser.write(line)
         time.sleep(1)
-
-ser.close()
